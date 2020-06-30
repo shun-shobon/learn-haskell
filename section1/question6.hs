e1109cb3eec300b98ed7cf9b69b238a8d1d1110b
shun-shobon <shun.shobon@gmail.com>
2020-06-30 21:26:20 +0900

    feat(section1): add question6

---
+++ section1/question6.hs
@@ -0,0 +1,10 @@
+perpPoint :: (Float, Float) -> (Float, Float, Float) -> (Float, Float)
+perpPoint (p, q) (a, b, c) = (x, y)
+    where
+        x = (a * c + b * d) / (a * a + b * b)
+        y = (b * c - a * d) / (a * a + b * b)
+        d = b * p - a * q
+
+main = do
+    print $ perpPoint (0, 2) (1, -1, 0)
+
