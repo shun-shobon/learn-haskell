d8b81f295b39a1543c50621a79fed0f102ec3daf
shun-shobon <shun.shobon@gmail.com>
2020-08-16 21:55:25 +0900

    feat(section3): add question7

---
+++ section3/question7.hs
@@ -0,0 +1,16 @@
+qsort :: (Ord a, Show a) => [a] -> IO [a]
+qsort []     = return []
+qsort (n:xs) = do
+    let
+        lt   = [x | x <- xs, x <  n]
+        gteq = [x | x <- xs, x >= n]
+    putStrLn $ "qsort " ++ show (n:xs) ++ " = qsort" ++
+        show lt ++ " ++ " ++ show [n] ++ " ++ " ++ show gteq
+    lt'   <- qsort lt
+    gteq' <- qsort gteq
+    return $ lt' ++ [n] ++ gteq'
+
+main :: IO ()
+main = do
+    print =<< qsort [4, 6, 9, 8, 3, 5, 1, 7, 2]
+
