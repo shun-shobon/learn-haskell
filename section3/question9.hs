372d4295e7ef314ac0c30f1d2a53a19559841f21
shun-shobon <shun.shobon@gmail.com>
2020-08-22 23:08:14 +0900

    feat(section3): add question9

---
+++ section3/question9.hs
@@ -0,0 +1,13 @@
+import Data.IORef
+
+main :: IO ()
+main = do
+    s <- newIORef 0
+    let loop i | i <= 100 = do
+            s' <- readIORef s
+            writeIORef s $ s' + i
+            loop $ i + 1
+        loop _ = return ()
+    loop 1
+    print =<< readIORef s
+
